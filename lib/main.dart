6e360da2824b61e396d1c11d31459b9abc6e9a05
Tjs87474 <Tsingleton14@student.gsu.edu>
2025-03-01 23:26:57 -0500

    Added methods

--- lib/main.dart
+++ lib/main.dart
@@ -29,13 +29,44 @@ class TaskListScreen extends StatefulWidget {
 
 class _TaskListScreenState extends State<TaskListScreen> {
   final List<Map<String, dynamic>> _tasks = [];
+  final TextEditingController _controller = TextEditingController();
+
+
+
+  // Method to add a new task
+  void _addTask() {
+    if (_controller.text.isNotEmpty) {
+      setState(() {
+        _tasks.add({
+          'task': _controller.text,
+          'isCompleted': false,
+        });
+      });
+      _controller.clear(); // Clear the input field after adding the task
+    }
+  }
+
+  // Method to toggle the completion status of a task
+  void _toggleTaskCompletion(int index) {
+    setState(() {
+      _tasks[index]['isCompleted'] = !_tasks[index]['isCompleted'];
+    });
+  }
+
+  // Method to remove a task
+  void _removeTask(int index) {
+    setState(() {
+      _tasks.removeAt(index);
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Task List')),
       body: Center(
-        child: Text('Task List Screen'),
+        child: Text(''),
       ),
     );
   }
