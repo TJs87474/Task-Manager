04871c7c64c1bea5ecc2464bf76a7f69c171f798
Tjs87474 <Tsingleton14@student.gsu.edu>
2025-03-01 23:09:22 -0500

    List added for tasks

--- lib/main.dart
+++ lib/main.dart
@@ -1,120 +1,41 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(TaskManagerApp());
+  runApp(const MyApp());
 }
 
-class TaskManagerApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Task Manager',
+      title: 'Task List App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
       ),
-      home: TaskListScreen(),
+      home: const TaskListScreen(),
     );
   }
 }
 
-class Task {
-  String name;
-  bool isCompleted;
-
-  Task({required this.name, this.isCompleted = false});
-}
-
 class TaskListScreen extends StatefulWidget {
+  const TaskListScreen({super.key});
+
   @override
   _TaskListScreenState createState() => _TaskListScreenState();
 }
 
 class _TaskListScreenState extends State<TaskListScreen> {
-  final List<Task> _tasks = [];
-  final TextEditingController _taskController = TextEditingController();
-
-  void _addTask() {
-    if (_taskController.text.isNotEmpty) {
-      setState(() {
-        _tasks.add(Task(name: _taskController.text));
-        _taskController.clear();
-      });
-    }
-  }
-
-  void _toggleTaskCompletion(int index) {
-    setState(() {
-      _tasks[index].isCompleted = !_tasks[index].isCompleted;
-    });
-  }
-
-  void _deleteTask(int index) {
-    setState(() {
-      _tasks.removeAt(index);
-    });
-  }
+  final List<Map<String, dynamic>> _tasks = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Task Manager'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: _taskController,
-                    decoration: InputDecoration(
-                      labelText: 'Enter Task',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                ),
-                SizedBox(width: 10),
-                ElevatedButton(
-                  onPressed: _addTask,
-                  child: Text('Add'),
-                ),
-              ],
-            ),
-            SizedBox(height: 20),
-            Expanded(
-              child: ListView.builder(
-                itemCount: _tasks.length,
-                itemBuilder: (context, index) {
-                  return Card(
-                    child: ListTile(
-                      leading: Checkbox(
-                        value: _tasks[index].isCompleted,
-                        onChanged: (value) {
-                          _toggleTaskCompletion(index);
-                        },
-                      ),
-                      title: Text(
-                        _tasks[index].name,
-                        style: TextStyle(
-                          decoration: _tasks[index].isCompleted
-                              ? TextDecoration.lineThrough
-                              : null,
-                        ),
-                      ),
-                      trailing: IconButton(
-                        icon: Icon(Icons.delete, color: Colors.red),
-                        onPressed: () => _deleteTask(index),
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
+      appBar: AppBar(title: const Text('Task List')),
+      body: Center(
+        child: Text('Task List Screen'),
       ),
     );
   }
